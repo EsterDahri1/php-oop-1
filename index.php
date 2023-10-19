f70d4c04a74366784870935b052e38bb7895274e
EsterDahri <esterdahri@gmail.com>
2023-10-19 16:25:20 +0200

    create complete laytou BONUS 2

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /*
-è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo
+è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo ✔️
 
 vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà ✔️
 
 Bonus 1: Modificare la classe Movie in modo che accetti piú di un genere.
-Bonus 2: Creare un layout completo per stampare a schermo una lista di movies.
+Bonus 2: Creare un layout completo per stampare a schermo una lista di movies. ✔️
 */
 
 # Class declaration
@@ -66,9 +66,9 @@ $movie_3->year = 2023;
 $movie_3->director = 'Domingo Gonzales';
 $movie_3->timeDuration = '117 min';
 
-var_dump($movie_1);
-var_dump($movie_2);
-var_dump($movie_3);
+// var_dump($movie_1);
+// var_dump($movie_2);
+// var_dump($movie_3);
 ?>
 
 <!DOCTYPE html>
@@ -78,10 +78,69 @@ var_dump($movie_3);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
-    <h1>MOVIES</h1>
+<body class="bg-info bg-opacity-25">
+    <div class="pt-5 container">
+        <h1 class="text-center text-primary">Movies</h1>
+        <div class="row">
+
+            <div class="col-4 card-group">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/100/80" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title text-center text-primary"><?= $movie_1->title ?></h4>
+                        <p class="card-text">
+                        <ul>
+                            <li><?= $movie_1->year ?></li>
+                            <li><?= $movie_1->director ?></li>
+                            <li><?= $movie_1->timeDuration ?></li>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 card-group">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/100/80" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title text-center text-primary"><?= $movie_2->title ?></h4>
+                        <p class="card-text">
+                        <ul>
+                            <li><?= $movie_2->year ?></li>
+                            <li><?= $movie_2->director ?></li>
+                            <li><?= $movie_2->timeDuration ?></li>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 card-group">
+                <div class="card">
+                    <img class="card-img-top" src="https://picsum.photos/100/80" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title text-center text-primary"><?= $movie_3->title ?></h4>
+                        <p class="card-text">
+                        <ul>
+                            <li><?= $movie_3->year ?></li>
+                            <li><?= $movie_3->director ?></li>
+                            <li><?= $movie_3->timeDuration ?></li>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
