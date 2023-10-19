a8e7c1c657b3e56152748448fdb39723b5b7146a
EsterDahri <esterdahri@gmail.com>
2023-10-19 13:08:17 +0200

    add f constructor & other functions

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 /*
 è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo
 
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà 
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà ✔️
+
+Bonus 1: Modificare la classe Movie in modo che accetti piú di un genere.
+Bonus 2: Creare un layout completo per stampare a schermo una lista di movies.
 */
 
 # Class declaration
@@ -12,12 +15,40 @@ class Movie
     public $year;
     public $director;
     public $timeDuration;
+
+
+    function __construct($_title, $_year, $_director, $_timeDuration)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->director = $_director;
+        $this->timeDuration = $_timeDuration;
+    }
+
+    public function getTitle()
+    {
+        echo $this->title;
+    }
+
+    public function getYear()
+    {
+        echo $this->year;
+    }
+
+    public function getDirector()
+    {
+        echo $this->director;
+    }
+    public function getTimeDuration()
+    {
+        echo $this->timeDuration;
+    }
 }
 
 # Create class istances
-$movie_1 = new Movie();
-$movie_2 = new Movie();
-$movie_3 = new Movie();
+$movie_1 = new Movie('Barbie', 2023, 'Greta Gerwig', '114 min');
+$movie_2 = new Movie('Avatar: The Way of Water', 2022, 'James Cameron', '192 min');
+$movie_3 = new Movie('Culpa mìa', 2023, 'Domingo Gonzales', '117 min');
 
 # Access to the object properties print on page
 $movie_1->title = 'Barbie';
