1962c7fe10a8c64a3d4f5f1447ff3598ca6c437d
EsterDahri <esterdahri@gmail.com>
2023-10-19 12:25:21 +0200

    add class variables and istances

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+/*
+è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo
+
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà 
+*/
+
+class Movie
+{
+    public $title;
+    public $year;
+    public $director;
+    public $timeDuration;
+}
+
+$movie_1 = new Movie();
+$movie_2 = new Movie();
+$movie_3 = new Movie();
